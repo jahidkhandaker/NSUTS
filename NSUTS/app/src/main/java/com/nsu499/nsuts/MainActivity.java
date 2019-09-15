46f45bc35f61c3ad9baa70aa7b1887bdd0b12e64
jahidkhandaker <jahid.khandaker@northsouth.edu>
2019-09-15 17:27:28 +0600

    Logout Added within MainActivity

--- NSUTS/app/src/main/java/com/nsu499/nsuts/MainActivity.java
+++ NSUTS/app/src/main/java/com/nsu499/nsuts/MainActivity.java
@@ -1,10 +1,12 @@
 package com.nsu499.nsuts;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -20,10 +22,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.AdapterView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+
+    private Button mLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,18 +54,34 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_gallery,
                 R.id.nav_slideshow,
                 R.id.nav_tools,
-                R.id.nav_share,
-                R.id.nav_send)
+                R.id.nav_share)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        //////---Hard Code Start Here------------------------------------------------------
+
+            mLogout= findViewById(R.id.nav_Logout);
+            mLogout.setOnClickListener(new NavigationView.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, LoginActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+
+
+
+        //////---Hard Code Ends Here------------------------------------------------------
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
