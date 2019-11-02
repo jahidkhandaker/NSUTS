7d8bed7eef4186da4669123041a2649e54049752
jahidkhandaker <jahid.khandaker@northsouth.edu>
2019-11-02 18:36:15 +0600

    Bus Arrival Notification Developed;

--- NSUTS/app/src/main/java/com/nsu499/nsuts/ui/home/HomeFragment.java
+++ NSUTS/app/src/main/java/com/nsu499/nsuts/ui/home/HomeFragment.java
@@ -1,8 +1,12 @@
 package com.nsu499.nsuts.ui.home;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +14,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 
 import androidx.lifecycle.ViewModelProviders;
@@ -36,6 +42,8 @@ import com.nsu499.nsuts.R;
 
 import java.util.Objects;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
 public class HomeFragment extends Fragment implements OnMapReadyCallback{
 
     private HomeViewModel homeViewModel;
@@ -46,9 +54,6 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
     private Marker busMarker;
 
     private FusedLocationProviderClient mFusedLocationProviderClient;
-    private LocationListener locationListener;
-    private LocationManager locationManager;
-    private Location location;
 
     DatabaseReference mLocationDatabase;
     private final long MIN_TIME=1000;
@@ -58,8 +63,9 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
     private LatLng nsu;
     private LatLng latLng;
 
-    private Double la;
-    private Double lo;
+    private final String Channel_ID = "personal_Notification";
+    private final int Notification_Id = 002;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -121,38 +127,24 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
         mLocationDatabase = FirebaseDatabase.getInstance().getReference().child("busId");
         mLocationDatabase.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange( DataSnapshot dataSnapshot) {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 //busMarker.remove();
+
                 mMap.clear();
-                double [] myLoc;
-                myLoc =  myLocation();
+                //myLocation();
+                //Toast.makeText(getActivity(), myLocation.getMyLat()+"/", Toast.LENGTH_SHORT).show();
                 if(dataSnapshot.exists()) {
-                    float bLat, bLon;
                     for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
                         String title = dataSnapshot1.getKey() ;
                         Double lat  = Double.valueOf(Objects.requireNonNull(dataSnapshot1.child("location").child("latitude").getValue(String.class)));
                         Double lon  = Double.valueOf(Objects.requireNonNull(dataSnapshot1.child("location").child("longitude").getValue(String.class)));
                         latLng = new LatLng(lat, lon);
-
-                        bLat = convertToFloat(lat);
-                        bLon = convertToFloat(lon);
-
                         busMarker =  mMap.addMarker(new MarkerOptions().position(latLng).title(title).icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher_bus_foreground)));
-                        if (title.equals(FixedBus[0])){
-                            float[] dist = new float[1];
-                            //location.distanceBetween(23.815455, 90.425264, 23.892020, 90.401956, dist);
-                            location.distanceBetween(myLoc[0], myLoc[1], bLat, bLon, dist);
-                            String dur = String.valueOf(dist[0] / 1000.00);
-                            Toast.makeText(getActivity(), dur, Toast.LENGTH_SHORT).show();
-//                            if ((dist[0])/1000 < 3){
-//                                Toast.makeText(getActivity(), "comming", Toast.LENGTH_LONG).show();
-//                            }
+                        busOnArrival(lat,lon,title,FixedBus[0]);
 
                         }
                     }
-
                 }
-            }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
@@ -163,7 +155,26 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
 
     }
 
-    private double[] myLocation() {
+
+//    public void myLocation() {
+//        final double [] myL = new double[2];
+//        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+//        mFusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+//            @Override
+//            public void onSuccess(Location location) {
+//                if (location != null) {
+//                    myL[0] = location.getLatitude();
+//                    myL[1]= location.getLongitude();
+//                    nsu = new LatLng(myL[0], myL[1]);
+//                    MeMarker =  mMap.addMarker(new MarkerOptions().position(nsu).title("Me"));
+//
+//                }
+//            }
+//        });
+//    }
+
+    private void busOnArrival(final Double lat,final Double lon,final String title, final String fixedBus) {
+//        Toast.makeText(getActivity(), myLocation.getMyLat()+"/" , Toast.LENGTH_SHORT).show();
         final double [] myL = new double[2];
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
         mFusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
@@ -171,17 +182,49 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
             public void onSuccess(Location location) {
                 if (location != null) {
                     myL[0] = location.getLatitude();
-                    myL[1]= location.getLongitude();
+                    myL[1] = location.getLongitude();
                     nsu = new LatLng(myL[0], myL[1]);
-                    MeMarker =  mMap.addMarker(new MarkerOptions().position(nsu).title("Me"));
+                    MeMarker = mMap.addMarker(new MarkerOptions().position(nsu).title("Me"));
+
+                    if (title.equals(fixedBus)) {
+                        float[] dist = new float[1];
+                        //location.distanceBetween(23.815455, 90.425264, 23.892020, 90.401956, dist);
+                        location.distanceBetween(myL[0], myL[1], lat, lon, dist);
+                        String dur = String.valueOf(dist[0] / 1000.00);
+                       // Toast.makeText(getActivity(), myL[0] + "/"+fixedBus+"/" + myL[1], Toast.LENGTH_SHORT).show();
+                        if ((dist[0]) < 3.00){
+                            //Toast.makeText(getActivity(), "comming", Toast.LENGTH_LONG).show();
+                            BusNotification();
+                        }else {
+                            //Toast.makeText(getActivity(), dist[0]+"nothing/"+dur, Toast.LENGTH_LONG).show();
+
+                        }
+
+                    }
                 }
             }
         });
-        return myL;
     }
 
-    public static Float convertToFloat(double doubleValue) {
-        return (float) doubleValue;
+    public void BusNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(Channel_ID, "busntfy", importance);
+            channel.setDescription("busntfy");
+            NotificationManager notificationManager = getActivity().getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getActivity(),Channel_ID);
+        builder.setSmallIcon(R.drawable.ic_menu_busbook);
+        builder.setContentTitle("Bus Arrival Alert");
+        builder.setContentText("Approximate time 5-10 minutes");
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getActivity());
+        notificationManagerCompat.notify(Notification_Id,builder.build());
     }
+
 
 }
+
